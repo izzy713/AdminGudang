30df2065358e5c84f8226dfd0fd2c0924f3fcd64
Adi Sucipto <asucipto871@gmail.com>
2022-10-26 18:04:29 +0700

    Change Log : - import file from another folder - edit databarang.html - fix parameter menu in header.html

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template
+import config.functions as function
+
+# print(function.coba())
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/main")
 def main() -> str:
-    return render_template("index.html", menu="main")
+    return render_template("index.html", menu="main", submenu='dashboard')
 
 @app.route("/inputbarang")
 def inputbarang() -> str:
-    return render_template("mod_barang/inputbarang.html", menu="barangmasuk", submenu="forminputbarang")
+    return render_template("mod_barang/inputbarang.html", menu="barang", submenu="forminputbarang")
 
 @app.route("/databarang")
 def databarang() -> str:
-    return render_template('mod_barang/databarang.html', menu="databarang", submenu="listdatabarang")
+    return render_template('mod_barang/databarang.html', menu="barang", submenu="listdatabarang")
 
 if __name__ == "__main__":
     app.run(debug=False)
